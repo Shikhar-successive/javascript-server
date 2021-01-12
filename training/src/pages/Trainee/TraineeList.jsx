986580245a5f363500415205238aa31c53f8586e
Shikhar-successive <shikhar.singh@successive.tech>
2021-01-12 18:04:49 +0530

    feat: add files of frontend Day 19

--- training/src/pages/Trainee/TraineeList.jsx
+++ training/src/pages/Trainee/TraineeList.jsx
@@ -194,8 +194,8 @@ class TraineeList extends Component {
             console.log(data.traineeCount);
             console.log(page, '---page 1');
           } else if (((data.traineeCount - 1) % 5) === 0) {
-            if ((data.traineeCount / 5) > 1 && page === this.page) {
-              console.log((data.traineeCount / 5));
+            if (!((data.traineeCount - 1) / 5 === page)) {
+              console.log(Math.round(data.traineeCount / 5) === page, '2');
               console.log(page);
               this.setState({
                 spinner: false,
@@ -205,8 +205,9 @@ class TraineeList extends Component {
               this.getTrainees();
               console.log(data.traineeCount);
               console.log(page, '---page 2');
-            } else if ((data.traineeCount / 5) && !(page === this.page)) {
-              console.log(traineeInfo);
+            } else if ((data.traineeCount - 1) / 5 === page) {
+              console.log(Math.round(data.traineeCount / 5) === page, '2aa');
+              console.log(page);
               this.setState({
                 spinner: false,
                 page: page -= 1,
@@ -214,8 +215,38 @@ class TraineeList extends Component {
               this.deleteDialogClose();
               this.getTrainees();
               console.log(data.traineeCount);
-              console.log(page, '---page 3run');
+              console.log(page, '---page 2aa');
             }
+            // if ((data.traineeCount / 5) > 1 && page === this.page) {
+            // if (((data.traineeCount / 5) === page)) {
+            //   console.log(Math.round(data.traineeCount / 5), '2');
+            //   console.log(page);
+            //   this.setState({
+            //     spinner: false,
+            //     page: page -= 1,
+            //   }, () => this.componentDidMount());
+            //   this.deleteDialogClose();
+            //   this.getTrainees();
+            //   console.log(data.traineeCount);
+            //   console.log(page, '---page 2');
+            // }
+            // if ((data.traineeCount / 5) === page) {
+            //   this.setState({
+            //     // spinner: false,
+            //     page: page -= 1,
+            //   }, () => this.componentDidMount());
+            // }
+            // } else if ((data.traineeCount / 5) && !(page === this.page)) {
+            //   console.log(traineeInfo);
+            //   this.setState({
+            //     spinner: false,
+            //     page: page -= 1,
+            //   }, () => this.componentDidMount());
+            //   this.deleteDialogClose();
+            //   this.getTrainees();
+            //   console.log(data.traineeCount);
+            //   console.log(page, '---page 3run');
+            // }
           }
         } else if (page === 0) {
           this.setState({
